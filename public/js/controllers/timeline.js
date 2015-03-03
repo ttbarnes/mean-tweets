4483241ab52ea0e8bae16c82301cc3814101aa8e
ttbarnes <tony@tonybarnes.me>
2015-03-03 08:32:45 +0000

    render api call to bears in timeline

--- public/js/controllers/timeline.js
+++ public/js/controllers/timeline.js
@@ -1,6 +1,13 @@
 angular.module('meanExampleApp').controller('TimelineCtrl', 
-  function($scope) {
+  function($scope, $http) {
 
     $scope.tagline = 'The square root of life is pi!';
+
+    $http({ method: 'GET', url: 'api/bears' }).
+      success(function(data, status, headers, config) {
+        $scope.bears = data;
+        console.log($scope.bears);
+      });
+
 
 });
