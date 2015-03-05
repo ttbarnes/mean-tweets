a866b69aa31985265249a085555ecc3a996145a9
ttbarnes <tony@tonybarnes.me>
2015-03-05 21:25:28 +0000

    post to api in timeline form

--- public/js/controllers/timeline.js
+++ public/js/controllers/timeline.js
@@ -1,11 +1,22 @@
 angular.module('meanExampleApp').controller('TimelineCtrl', 
   function($scope, Restangular) {
 
-    var bears = Restangular.all('api/bears');
+    var baseBears = Restangular.all('api/bears');
 
-    bears.getList().then(function(bears) {
+    baseBears.getList().then(function(bears) {
       $scope.bears = bears;
     });
+
+    $scope.bears = baseBears.getList().$object;
+
+    $scope.postTweet = function(tweet){
+
+      var newTweet = {
+        name: tweet.content
+      };
+
+      baseBears.post(newTweet);
+    };
 
 
 
