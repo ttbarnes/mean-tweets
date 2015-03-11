b98cc3224415df2f2598b5db19ba5e621233f131
ttbarnes <tony@tonybarnes.me>
2015-03-11 22:30:01 +0000

    move public files into public/src

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse applica
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public/src')); // set the static files location
 
 // ROUTES FOR OUR API
 // =============================================================================
