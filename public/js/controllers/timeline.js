99a5ffc1357853a41c8f1c7d01e87294852ee9cf
ttbarnes <tony@tonybarnes.me>
2015-03-11 21:48:24 +0000

    remove timeline tweets

--- public/js/controllers/timeline.js
+++ public/js/controllers/timeline.js
@@ -1,20 +1,6 @@
 angular.module('meanExampleApp').controller('TimelineCtrl', 
   function($scope, Restangular) {
 
-    (function getAllTweets(){
-      baseTweets = Restangular.all('api/tweets');
-    })();
-
-    baseTweets.getList().then(function(tweets) {
-      $scope.tweets = tweets;
-    });
-
-    function tweetsObject() { 
-      $scope.tweets = baseTweets.getList().$object;
-    };
-
-    setInterval(tweetsObject, 10000);
-
     $scope.postTweet = function(tweet){
 
       var newTweet = {
