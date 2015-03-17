b3fd2a90cc1282ce345292ee367810414368f1dc
ttbarnes <tony@tonybarnes.me>
2015-03-17 18:33:42 +0000

    tweets controller unit test

---
+++ public/test/unit/controllers/tweets.js
@@ -0,0 +1,24 @@
+describe('TweetsCtrl', function() {
+
+  var $q;
+  var scope;
+  var target;
+
+  beforeEach(function() {
+    module('meanExampleApp');
+
+    inject(function($injector) {
+      $q = $injector.get('$q');
+      var $controller = $injector.get('$controller');
+      scope = $injector.get('$rootScope').$new();
+      target = $controller('TweetsCtrl', { $scope: scope });
+    });
+
+  });
+
+  it('should have tweets variable', function(){
+    expect(scope.tweets).toBeDefined();
+  });
+
+
+});
