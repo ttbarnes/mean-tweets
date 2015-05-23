12beeb57d0990f68432d09d557ac19351052977a
ttbarnes <tony@tonybarnes.me>
2015-05-23 15:16:25 +0100

    only show tweets and post if authenticated

--- public/src/js/controllers/tweets.js
+++ public/src/js/controllers/tweets.js
@@ -8,21 +8,27 @@ angular.module('meanExampleApp').controller('TweetsCtrl',
     });
 
     $scope.postTweet = function(tweet){
+      if(auth.isAuthenticated) {
+        var newTweet = {
+          name: tweet.content,
+          username: auth.profile.nickname
+        };
 
-      var newTweet = {
-        name: tweet.content,
-        username: auth.profile.nickname
-      };
+        tweetsService.tweets.post(newTweet).then(function(){
+          console.info('posted a tweet', newTweet);
+          //todo: clear the form input
+        });
 
-      tweetsService.tweets.post(newTweet).then(function(){
-        console.info('posted a tweet', newTweet);
-        //todo: clear the form input
-      });
+        //todo: handle errors
 
-      tweetsService.tweets.getList().then(function (tweets){
-        $scope.tweets = tweets;
-        console.info('got new tweets');
-      });
+        tweetsService.tweets.getList().then(function (tweets){
+          $scope.tweets = tweets;
+          console.info('got new tweets');
+        });
+      }
+      else {
+        console.error('unable to post tweet - user is not authenticated');
+      }
 
     };
 
