3b18ed65188e883a8431dbc61239d48772753a9e
ttbarnes <tony@tonybarnes.me>
2015-06-16 15:06:00 +0100

    tweets factory function for user specific queries

--- public/src/js/services/tweets.js
+++ public/src/js/services/tweets.js
@@ -1,7 +1,17 @@
+//usage eg:
+//
+//tweetsService.tweetsTest.getList().then(function (tweets){ ... });
+//
+//tweetsService.userSpecificTweets('support2').getList().then(function (tweets){ ... });
+//
+
 angular.module('meanExampleApp').factory('tweetsService', function (Restangular){
 
     return {
-      tweets: Restangular.all('api/tweets')
+      tweets: Restangular.all('api/tweets'),
+      userSpecificTweets: function(username) {
+        return Restangular.all('api/tweets/' + username);
+      }
     }
 
   });
