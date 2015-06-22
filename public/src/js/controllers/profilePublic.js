53e33a576db0e87a7af12d35e69fb50639710d3a
ttbarnes <tony@tonybarnes.me>
2015-06-22 14:31:33 +0100

    follow button, placeholder function

--- public/src/js/controllers/profilePublic.js
+++ public/src/js/controllers/profilePublic.js
@@ -1,11 +1,20 @@
-angular.module('meanExampleApp').controller('ProfilePublicCtrl', function ($stateParams, $scope, Restangular, tweetsService) {
+angular.module('meanExampleApp').controller('ProfilePublicCtrl', function (auth, $stateParams, $scope, Restangular, tweetsService) {
 
   $scope.username = $stateParams.username;
+
+  if($stateParams.username === auth.profile.nickname){
+    $scope.usersOwnProfile = true;
+  }
 
   //todo: add promise, errors, no user, etc - maybe use resolve() in routes?
 
   tweetsService.userSpecificTweets($stateParams.username).getList().then(function (tweets){
     $scope.tweets = tweets;
   });
+
+
+  $scope.followUser = function(username) {
+    console.log('todo: follow user: ', username);
+  };
 
 });
