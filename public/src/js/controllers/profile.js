28b4b58eb03e7058e81de4f7e02b3fa4d478bb9b
ttbarnes <tony@tonybarnes.me>
2015-07-04 18:38:08 +0100

    bind existing user profileDetails in form

--- public/src/js/controllers/profile.js
+++ public/src/js/controllers/profile.js
@@ -1,4 +1,4 @@
-angular.module('meanExampleApp').controller('ProfileCtrl', function (auth, $state, $stateParams, $scope, Restangular) {
+angular.module('meanExampleApp').controller('ProfileCtrl', function (auth, $state, $stateParams, $scope, Restangular, userProfileService) {
 
   //temp solution to redirect if user is not auth
   if(!auth.isAuthenticated && $state.$current.data.auth.required === true) {
@@ -7,6 +7,10 @@ angular.module('meanExampleApp').controller('ProfileCtrl', function (auth, $stat
 
   $scope.loggedInUser = auth.profile.nickname;
 
+  userProfileService.user($scope.loggedInUser).getList().then(function (profile){
+    $scope.profile = profile;
+    $scope.profileDetails = $scope.profile[0].details[0];
+  });
 
   $scope.updateProfileDetails = function(data) {
 
