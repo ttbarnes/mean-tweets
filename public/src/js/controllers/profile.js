2e9dd365af58630656beac282371a0483dcfe95b
ttbarnes <tony@tonybarnes.me>
2015-07-04 17:52:40 +0100

    edit profile details object, restangular call

--- public/src/js/controllers/profile.js
+++ public/src/js/controllers/profile.js
@@ -1,20 +1,30 @@
-angular.module('meanExampleApp').controller('ProfileCtrl', function (auth, $state, $stateParams, $scope) {
-
+angular.module('meanExampleApp').controller('ProfileCtrl', function (auth, $state, $stateParams, $scope, Restangular) {
 
   //temp solution to redirect if user is not auth
   if(!auth.isAuthenticated && $state.$current.data.auth.required === true) {
     $state.go('loginRequired');
   }
 
+  $scope.loggedInUser = auth.profile.nickname;
+
+
   $scope.updateProfileDetails = function(data) {
 
-    var profileDetails = {
-      websiteUrl : data.websiteUrl,
-      location   : data.location,
-      about      : data.about
-    };
+    if(auth.isAuthenticated) {
 
-    console.log(profileDetails);
+      var profileDetails = {
+        websiteUrl : data.websiteUrl,
+        location   : data.location,
+        about      : data.about
+      };
+
+      console.log(profileDetails);
+
+      Restangular.all('api/profiles/' + $scope.loggedInUser + '/details').customPUT(profileDetails).then(function () {
+        console.log($scope.loggedInUser + 'just updated their profile details');
+        console.log('posted to: ' + 'api/profiles/' + $scope.loggedInUser + '/details');
+      });
+    }
 
   }
 
