914f649a7aebd0b9bf82f528ef6cc2de88bd3189
ttbarnes <tony@tonybarnes.me>
2015-07-05 14:29:28 +0100

    remove redirect from see you soon

--- public/src/js/controllers/seeYouSoon.js
+++ public/src/js/controllers/seeYouSoon.js
@@ -1,10 +1,3 @@
-angular.module('meanExampleApp').controller('SeeYouSoonCtrl', function ($timeout, $state) {
-
-  var seconds = 5000;
-
-  $timeout(function(){
-    console.log('ok...');
-    $state.go('home');
-  }, seconds);
+angular.module('meanExampleApp').controller('SeeYouSoonCtrl', function () {
 
 });
