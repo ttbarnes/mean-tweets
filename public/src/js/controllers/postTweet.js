c381ca57acc6f23f362ba764b80977548699be92
ttbarnes <tony@tonybarnes.me>
2015-07-07 00:00:06 +0100

    sepearte postTweet from timeline

---
+++ public/src/js/controllers/postTweet.js
@@ -0,0 +1,26 @@
+angular.module('meanExampleApp').controller('PostTweetCtrl', 
+  function ($scope, Restangular, currentUserFactory, tweetsFactory) {
+
+    $scope.postTweet = function(tweet){
+      console.log('cliked postTweet!');
+      if(currentUserFactory.isAuth) {
+        var newTweet = {
+          username: currentUserFactory.username,
+          copy: tweet.copy,
+          image:{
+            url:((tweet.image) ? tweet.image.url : '')
+          }
+        };
+
+        tweetsFactory.tweets.post(newTweet).then(function(){
+          console.info('posted a tweet', newTweet);
+          $scope.tweet = '';
+          //getTweets();
+        });
+      }
+      else {
+        console.error('unable to post tweet - user is not authenticated');
+      }
+    };
+
+});
--- public/src/js/controllers/timeline.js
+++ public/src/js/controllers/timeline.js
@@ -69,27 +69,6 @@ angular.module('meanExampleApp').controller('TimelineCtrl',
 
     getTweets();
 
-    $scope.postTweet = function(tweet){
-      if(currentUserFactory.isAuth) {
-        var newTweet = {
-          username: currentUserFactory.username,
-          copy: tweet.copy,
-          image:{
-            url:((tweet.image) ? tweet.image.url : '')
-          }
-        };
-
-        tweetsFactory.tweets.post(newTweet).then(function(){
-          console.info('posted a tweet', newTweet);
-          $scope.tweet = '';
-          getTweets();
-        });
-      }
-      else {
-        console.error('unable to post tweet - user is not authenticated');
-      }
-    };
-
     $scope.favouriteTweet = function(tweetId) {
       if(currentUserFactory.isAuth) {
 
