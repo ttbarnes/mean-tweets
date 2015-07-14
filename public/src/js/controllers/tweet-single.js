7c2f4d3ec01412ce093ada32c3e4f6d0f2d6d2fd
ttbarnes <tony@tonybarnes.me>
2015-07-14 11:22:41 +0100

    tweet not found from view

--- public/src/js/controllers/tweet-single.js
+++ public/src/js/controllers/tweet-single.js
@@ -9,6 +9,9 @@ angular.module('meanTweetsApp').controller('TweetSingleCtrl',
       tweetsFactory.singleTweet(tweetId).get().then(function (tweet){
         $scope.tweet = tweet;
         console.info('got the tweet');
+      }, function (err){
+        console.warn('oh no, tweet could not be found! details: \n', err);
+        $scope.tweetNotFound = true;
       });
     };
 
