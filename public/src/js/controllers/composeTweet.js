0b82d5d910320e7d93a5b463ba48dcd65d3ed144
ttbarnes <tony@tonybarnes.me>
2015-07-20 17:43:33 +0100

    remove auth check from postTweet

--- public/src/js/controllers/composeTweet.js
+++ public/src/js/controllers/composeTweet.js
@@ -2,25 +2,22 @@ angular.module('meanTweetsApp').controller('ComposeTweetCtrl',
   function ($rootScope, $scope, Restangular, currentUserFactory, tweetsFactory, ngDialog) {
 
     $scope.postTweet = function(tweet){
-      if(currentUserFactory.isAuth) {
-        var newTweet = {
-          username: currentUserFactory.username,
-          copy: tweet.copy,
-          image:{
-            url:((tweet.image) ? tweet.image.url : '')
-          }
-        };
+      this.newTweet = {
+        username: currentUserFactory.username,
+        copy: tweet.copy,
+        image:{
+          url:((tweet.image) ? tweet.image.url : '')
+        }
+      };
 
-        tweetsFactory.tweets.post(newTweet).then(function(){
-          console.info('posted a tweet', newTweet);
-          $scope.tweet = '';
-          ngDialog.closeAll();
-          $rootScope.$broadcast('refreshTweets');
-        });
-      }
-      else {
-        console.error('unable to post tweet - user is not authenticated.');
-      }
+      var newTweet = this.newTweet;
+
+      tweetsFactory.tweets.post(newTweet).then(function(){
+        console.info('posted a tweet', newTweet);
+        $scope.tweet = '';
+        ngDialog.closeAll();
+        $rootScope.$broadcast('refreshTweets');
+      });
     };
 
 });
