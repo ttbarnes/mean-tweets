0fef7936300c9897cabaa2d6b7b9ee2d132f5be0
ttbarnes <tony@tonybarnes.me>
2015-07-20 15:47:23 +0100

    remove auth check from favouriteTweet functions

--- public/src/js/controllers/tweetFavourite.js
+++ public/src/js/controllers/tweetFavourite.js
@@ -1,39 +1,35 @@
 angular.module('meanTweetsApp').controller('TweetFavouriteCtrl', function (currentUserFactory, userProfileFactory, tweetsFactory, $scope) {
 
   $scope.favouriteTweet = function(tweetId) {
-    if(currentUserFactory.isAuth) {
 
-      var newFavourite = {
-        username: currentUserFactory.username
-      };
+    var newFavourite = {
+      username: currentUserFactory.username
+    };
 
-      //PUT the newFavourite username into the tweet's favourite array
-      tweetsFactory.favourites(tweetId).customPUT(newFavourite).then(function () {
-        console.log('posted new favourite tweet: ' + tweetId);
-      });
+    //PUT the newFavourite username into the tweet's favourite array
+    tweetsFactory.favourites(tweetId).customPUT(newFavourite).then(function () {
+      console.log('posted new favourite tweet: ' + tweetId);
+    });
 
-      //PUT tweet id in loggedInUser's profile favourites object
-      userProfileFactory.favourites(currentUserFactory.username, tweetId).customPUT(newFavourite).then(function () {
-        console.log('posted new favourite tweet to ' + currentUserFactory.username + '\'s profile');
-      });
+    //PUT tweet id in loggedInUser's profile favourites object
+    userProfileFactory.favourites(currentUserFactory.username, tweetId).customPUT(newFavourite).then(function () {
+      console.log('posted new favourite tweet to ' + currentUserFactory.username + '\'s profile');
+    });
 
-    }
   };
 
   $scope.unFavouriteTweet = function(tweetId, favouriteId) {
-    if(currentUserFactory.isAuth) {
 
-      //REMOVE the username's fav ID from the tweet's favourite array
-      tweetsFactory.singleFavourite(tweetId, favouriteId).remove().then(function () {
-        console.log('removed user\'s favourite tweet from the tweet: ' + tweetId);
-      });
+    //REMOVE the username's fav ID from the tweet's favourite array
+    tweetsFactory.singleFavourite(tweetId, favouriteId).remove().then(function () {
+      console.log('removed user\'s favourite tweet from the tweet: ' + tweetId);
+    });
 
-      //REMOVE the tweet ID from the user's profile favourites array
-      userProfileFactory.favourites(currentUserFactory.username, tweetId).remove().then(function () {
-        console.log('removed favourite tweet from ' + currentUserFactory.username + '\'s profile' );
-      });
+    //REMOVE the tweet ID from the user's profile favourites array
+    userProfileFactory.favourites(currentUserFactory.username, tweetId).remove().then(function () {
+      console.log('removed favourite tweet from ' + currentUserFactory.username + '\'s profile' );
+    });
 
-    }
   };
 
 });
