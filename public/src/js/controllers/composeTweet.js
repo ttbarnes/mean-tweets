008631ec13c67e5c9c948cdf4c9fb0586b0fee46
ttbarnes <tony@tonybarnes.me>
2015-07-21 20:54:27 +0100

    compose tweet loading icon

--- public/src/js/controllers/composeTweet.js
+++ public/src/js/controllers/composeTweet.js
@@ -2,6 +2,9 @@ angular.module('meanTweetsApp').controller('ComposeTweetCtrl',
   function ($rootScope, $scope, Restangular, currentUserFactory, apiEndpointFactory, ngDialog) {
 
     $scope.postTweet = function(tweet){
+
+      $scope.loading = true;
+
       this.newTweet = {
         username: currentUserFactory.username,
         copy: tweet.copy,
@@ -13,6 +16,8 @@ angular.module('meanTweetsApp').controller('ComposeTweetCtrl',
       var newTweet = this.newTweet;
 
       apiEndpointFactory.tweets.post(newTweet).then(function(){
+        $scope.loading = false;
+        $scope.loadingSuccess = true;
         console.info('posted a tweet', newTweet);
         $scope.tweet = '';
         ngDialog.closeAll();
