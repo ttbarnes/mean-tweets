262db56aba21a9be12a6edbb233ece8e50725015
ttbarnes <tony@tonybarnes.me>
2015-07-22 10:08:41 +0100

    GlobalCtrl unit tests

--- public/test/unit/controllers/global.js
+++ public/test/unit/controllers/global.js
@@ -11,14 +11,57 @@ describe('GlobalCtrl', function() {
       $q = $injector.get('$q');
       var $controller = $injector.get('$controller');
       scope = $injector.get('$rootScope').$new();
-      target = $controller('GlobalCtrl', { $scope: scope });
+      auth = $injector.get('auth');
+      httpBackend = $injector.get('$httpBackend');
+      ngDialog = $injector.get('ngDialog');
+      target = $controller('GlobalCtrl', { 
+        $scope: scope
+      });
     });
+
+    //prevents 'unexpected request: GET views/*/index.html' error
+    httpBackend.whenGET(/views.*/).respond(200, '');
 
   });
 
   it('should have the correct app name', function(){
     expect(scope.appName).toEqual('Mean tweets');
   });
+
+  it('should have the correct error heading copy', function(){
+    expect(scope.errorHeading).toEqual('Whoops!');
+  });
+
+  it('should have auth defined', function(){
+    expect(auth).toBeDefined();
+  });
+
+  it('should have a login or signup function', function(){
+    expect(scope.loginOrSignup).toBeDefined();
+  });
+
+  it('should excute login or signup function when called', function(){
+    spyOn(scope, 'loginOrSignup');
+    scope.loginOrSignup();
+    scope.$digest();
+    expect(scope.loginOrSignup).toHaveBeenCalled();
+  });
+
+  it('should have a compose tweet function', function(){
+    expect(scope.composeTweet).toBeDefined();
+  });
+
+  it('should excute compose tweet function when called', function(){
+    spyOn(scope, 'composeTweet');
+    scope.composeTweet();
+    scope.$digest();
+    expect(scope.composeTweet).toHaveBeenCalled();
+  });
+
+
+  //how to test ngDialog call?
+  //nothing seems to work.
+
 
 
 });
