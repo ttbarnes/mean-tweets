73041b2c28e802c4a4585c92a32caa35f83361d9
ttbarnes <tony@tonybarnes.me>
2015-07-23 19:50:27 +0100

    clean

--- public/test/unit/controllers/profilePublicSuccess.js
+++ public/test/unit/controllers/profilePublicSuccess.js
@@ -71,23 +71,6 @@ describe('ProfilePublicCtrl - success: user found', function() {
     expect(scope.loggedInUser).toEqual(currentUserFactory.username);
   });
 
-  describe('if profileUsername is the same as loggedInUser', function(){
-
-    beforeEach(function(){
-      scope.profileUsername = currentUserUsername;
-      httpBackend.flush();
-      scope.$digest();
-    });
-
-    //why doesn't this work?
-    /*
-    it('should declare usersOwnProfile = true', function(){
-      expect(scope.usersOwnProfile).toBeTruthy();
-    });
-    */
-
-  });
-
   describe('delete tweet', function(){
 
     beforeEach(function(){
@@ -104,68 +87,5 @@ describe('ProfilePublicCtrl - success: user found', function() {
     });
 
   });
-
-  /*
-  describe('initilisation', function() {
-
-    it('should have the same username in scope and state params', function() {
-      expect(scope.profileUsername).toEqual(stateParamsUsername);
-    });
-
-    it('should have profileUsernameData username in scope', function() {
-      expect(scope.profileUsernameData.username).toBeDefined();
-    });
-
-    it('should have undefined userNotFound', function() {
-      expect(scope.userNotFound).toBeFalsy();
-    });
-
-  });
-
-  //todo: test fof no user found.
-  //how to $provide ui-route resolve in jasmine describe blocks?
-  //describe('when/if user has not been found', function() {   });
-
-  describe('when/if user has been found', function() {
-
-    it('should render profileUsernameData in scope', function() {
-      expect(scope.profileUsernameData).toBeDefined();
-      expect(scope.profileUsernameData).toEqual(profileUsernameDataMockSuccess);
-      expect(scope.profileUsernameData.profile[0].details).toBeDefined();
-      expect(scope.profileUsernameData.profile[0].favourites).toBeDefined();
-      expect(scope.profileUsernameData.profile[0].followers).toBeDefined();
-      expect(scope.profileUsernameData.profile[0].following).toBeDefined();
-      expect(scope.profileUsernameData.success).toBeDefined();
-    });
-
-    it('should render profileUsernameData.profile in scope', function() {
-      expect(scope.profileUser).toEqual(profileUsernameDataMockSuccess.profile);
-      expect(scope.profileUser[0].details).toBeDefined();
-      expect(scope.profileUser[0].favourites).toBeDefined();
-      expect(scope.profileUser[0].followers).toBeDefined();
-      expect(scope.profileUser[0].following).toBeDefined();
-    });
-
-    describe('if current user is logged in', function() {
-
-      it('should have true isAuth', function() {
-        expect(currentUserFactory.isAuth).toBeTruthy();
-      });
-
-      it('should render the user\'s username in scope', function() {
-        expect(scope.loggedInUser).toEqual(currentUserFactory.username);
-      });
-
-      describe('if current logged in user is the same as public profile username ', function() {
-
-        //expect(scope.usersOwnProfile).toBeTruthy();
-
-      });
-
-    });
-a
-
-  });
-  */
 
 });
