cb03c431f389ed097ef28fda4536e8d8f777f55f
ttbarnes <tony@tonybarnes.me>
2015-07-23 18:23:56 +0100

    clean public profile failure unit test

--- public/test/unit/controllers/profilePublicFailure.js
+++ public/test/unit/controllers/profilePublicFailure.js
@@ -9,37 +9,11 @@ describe('ProfilePublicCtrl - failure: user not found', function() {
   var stateParamsUsername = state.params[0].username;
 
   var currentUserUsername = 'phillip';
-  
-  /*
-  var profileUsernameDataMockSuccess = {
-    profile: [{
-      details: [{
-        _id: '1234',
-        about: 'I love tweeting about things',
-        location: 'Newcastle',
-        websiteUrl: 'http://steven.newcastle.com'
-      }],
-      favourites: [],
-      followers: [],
-      following: []
-    }],
-    username: currentUserUsername,
-    success: true
-  };
-  */
 
   var profileUsernameDataMockFailure = {
     username: stateParamsUsername,
     success: false
   };
-
-
-  /*
-  var currentUserFactoryMockSuccess = {
-    isAuth : true,
-    username : currentUserUsername
-  };
-  */
 
   beforeEach(function() {
 
@@ -58,12 +32,7 @@ describe('ProfilePublicCtrl - failure: user not found', function() {
 
       ctrl = $controller('ProfilePublicCtrl', { 
         $scope: scope
-        //currentUserFactory: currentUserFactory
       });
-
-      //currentUserFactory = currentUserFactoryMockSuccess;
-
-      //scope.loggedInUser = currentUserFactory.username;
 
       scope.profileUsername = stateParamsUsername;
 
@@ -94,68 +63,5 @@ describe('ProfilePublicCtrl - failure: user not found', function() {
   it('should have true user not found', function(){
     expect(scope.userNotFound).toBeTruthy();
   });
-
-  /*
-  describe('initilisation', function() {
-
-    it('should have the same username in scope and state params', function() {
-      expect(scope.profileUsername).toEqual(stateParamsUsername);
-    });
-
-    it('should have profileUsernameData username in scope', function() {
-      expect(scope.profileUsernameData.username).toBeDefined();
-    });
-
-    it('should have undefined userNotFound', function() {
-      expect(scope.userNotFound).toBeFalsy();
-    });
-
-  });
-
-  //todo: test fof no user found.
-  //how to $provide ui-route resolve in jasmine describe blocks?
-  //describe('when/if user has not been found', function() {   });
-
-  describe('when/if user has been found', function() {
-
-    it('should render profileUsernameData in scope', function() {
-      expect(scope.profileUsernameData).toBeDefined();
-      expect(scope.profileUsernameData).toEqual(profileUsernameDataMockSuccess);
-      expect(scope.profileUsernameData.profile[0].details).toBeDefined();
-      expect(scope.profileUsernameData.profile[0].favourites).toBeDefined();
-      expect(scope.profileUsernameData.profile[0].followers).toBeDefined();
-      expect(scope.profileUsernameData.profile[0].following).toBeDefined();
-      expect(scope.profileUsernameData.success).toBeDefined();
-    });
-
-    it('should render profileUsernameData.profile in scope', function() {
-      expect(scope.profileUser).toEqual(profileUsernameDataMockSuccess.profile);
-      expect(scope.profileUser[0].details).toBeDefined();
-      expect(scope.profileUser[0].favourites).toBeDefined();
-      expect(scope.profileUser[0].followers).toBeDefined();
-      expect(scope.profileUser[0].following).toBeDefined();
-    });
-
-    describe('if current user is logged in', function() {
-
-      it('should have true isAuth', function() {
-        expect(currentUserFactory.isAuth).toBeTruthy();
-      });
-
-      it('should render the user\'s username in scope', function() {
-        expect(scope.loggedInUser).toEqual(currentUserFactory.username);
-      });
-
-      describe('if current logged in user is the same as public profile username ', function() {
-
-        //expect(scope.usersOwnProfile).toBeTruthy();
-
-      });
-
-    });
-a
-
-  });
-  */
 
 });
