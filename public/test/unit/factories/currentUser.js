4eca5f6211803fe14c820b0d1b11255c7430ec42
ttbarnes <tony@tonybarnes.me>
2015-07-24 08:54:16 +0100

    currentUserFactory auth success unit tests

--- public/test/unit/factories/currentUser.js
+++ public/test/unit/factories/currentUser.js
@@ -7,39 +7,74 @@ describe('currentUserFactory', function() {
       $q = $injector.get('$q');
       httpBackend = $injector.get('$httpBackend');
       store = $injector.get('store');
+      auth = $injector.get('auth');
 
       httpBackend.whenGET(/views.*/).respond(200, '');
     });
 
   });
 
-  describe('initialisation', function() {
+  describe('when unauthorised', function() {
 
     it('should return false auth check', inject(function (auth){
       expect(auth.isAuthenticated).toBeFalsy();
+    }));
+
+    it('should return false profile', inject(function (auth){
       expect(auth.profile).toBeFalsy();
     }));
 
     it('should return false', inject(function (currentUserFactory){
       expect(currentUserFactory).toBeFalsy();
+    }));
+
+    it('should return false isAuth', inject(function (currentUserFactory){
       expect(currentUserFactory.isAuth).toBeFalsy();
+      expect(currentUserFactory.user).toBeFalsy();
+    }));
+
+    it('should return false user', inject(function (currentUserFactory){
       expect(currentUserFactory.user).toBeFalsy();
     }));
 
   });
 
-/*
-  describe('when authenticated', function() {
+  describe('when authorised', function(){
 
+    beforeEach(function(){
 
-    it('should return true', inject(function (auth, store, jwtHelper, currentUserFactory){
+      mockUsername = 'jeff';
 
+      auth.isAuthenticated = true;
+
+      auth.profile = {
+        nickname: mockUsername
+      }
+
+    });
+
+    it('should do this that and the other', inject(function (currentUserFactory){
       expect(currentUserFactory.isAuth).toBeTruthy();
-      expect(currentUserFactory.username).toEqual('tomas');
     }));
 
-  });
-*/
+    it('shuld have mockUsername defined', inject(function (currentUserFactory){
+      expect(authUsername).toBeDefined();
+      expect(authUsername).toEqual(mockUsername);
+    }));
 
+    describe('user object creation', function(){
+
+      it('should create user object with isAuth', inject(function (currentUserFactory) {
+        expect(currentUserFactory.isAuth).toBeTruthy();
+      }));
+
+      it('should create user object with username', inject(function (currentUserFactory) {
+        expect(currentUserFactory.username).toBeDefined();
+        expect(currentUserFactory.username).toEqual(mockUsername);
+      }));
+
+    });
+
+  });
 
 });
