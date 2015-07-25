091725ea16be382eb38858287d940b051e8a9737
ttbarnes <tony@tonybarnes.me>
2015-07-25 12:29:20 +0100

    GlobalCtrl - test ngDialog open method call

--- public/test/unit/controllers/global.js
+++ public/test/unit/controllers/global.js
@@ -3,6 +3,7 @@ describe('GlobalCtrl', function() {
   var $q;
   var scope;
   var target;
+  var ngDialog;
 
   beforeEach(function() {
     module('meanTweetsApp');
@@ -13,9 +14,14 @@ describe('GlobalCtrl', function() {
       scope = $injector.get('$rootScope').$new();
       auth = $injector.get('auth');
       httpBackend = $injector.get('$httpBackend');
-      ngDialog = $injector.get('ngDialog');
+
+      ngDialog = {
+        open: jasmine.createSpy('ngDialog.open').and.callThrough()
+      };
+
       target = $controller('GlobalCtrl', { 
-        $scope: scope
+        $scope: scope,
+        ngDialog: ngDialog
       });
     });
 
@@ -23,44 +29,53 @@ describe('GlobalCtrl', function() {
 
   });
 
-  it('should have the correct app name', function(){
-    expect(scope.appName).toEqual('Mean tweets');
+  describe('initialization', function(){
+
+    it('should have the correct app name', function(){
+      expect(scope.appName).toEqual('Mean tweets');
+    });
+
+    it('should have the correct error heading copy', function(){
+      expect(scope.errorHeading).toEqual('Whoops!');
+    });
+
+    it('should have auth defined', function(){
+      expect(auth).toBeDefined();
+    });
+
+    it('should have a login or signup function', function(){
+      expect(scope.loginOrSignup).toBeDefined();
+    });
+
+    it('should excute login or signup function when called', function(){
+      spyOn(scope, 'loginOrSignup');
+      scope.loginOrSignup();
+      scope.$digest();
+      expect(scope.loginOrSignup).toHaveBeenCalled();
+    });
+
+    it('should have a compose tweet function', function(){
+      expect(scope.composeTweet).toBeDefined();
+    });
+
   });
 
-  it('should have the correct error heading copy', function(){
-    expect(scope.errorHeading).toEqual('Whoops!');
+  describe('compose tweet function', function(){
+
+    beforeEach(function(){
+      spyOn(scope, 'composeTweet').and.callThrough();
+      scope.composeTweet();
+      scope.$digest();
+    });
+    
+    it('should excute compose tweet function when called', function(){
+      expect(scope.composeTweet).toHaveBeenCalled();
+    });
+
+    it('should call ngDialog open method', function(){
+      expect(ngDialog.open).toHaveBeenCalled();
+    });
+
   });
-
-  it('should have auth defined', function(){
-    expect(auth).toBeDefined();
-  });
-
-  it('should have a login or signup function', function(){
-    expect(scope.loginOrSignup).toBeDefined();
-  });
-
-  it('should excute login or signup function when called', function(){
-    spyOn(scope, 'loginOrSignup');
-    scope.loginOrSignup();
-    scope.$digest();
-    expect(scope.loginOrSignup).toHaveBeenCalled();
-  });
-
-  it('should have a compose tweet function', function(){
-    expect(scope.composeTweet).toBeDefined();
-  });
-
-  it('should excute compose tweet function when called', function(){
-    spyOn(scope, 'composeTweet');
-    scope.composeTweet();
-    scope.$digest();
-    expect(scope.composeTweet).toHaveBeenCalled();
-  });
-
-
-  //how to test ngDialog call?
-  //nothing seems to work.
-
-
 
 });
