393c9665f48f38c3706886cd42f6b9b53b084313
ttbarnes <tony@tonybarnes.me>
2015-07-26 12:56:21 +0100

    followUser unit test di/mocks

--- public/test/unit/controllers/followUser.js
+++ public/test/unit/controllers/followUser.js
@@ -2,26 +2,42 @@ describe('FollowUserCtrl', function() {
 
   var $q;
   var scope;
-  var target;
+  var ctrl;
+
+  var currentUserFactoryMockSuccess = {
+    isAuth : true,
+    username : 'wally'
+  };
+
 
   beforeEach(function() {
-    module('FollowUserCtrl');
+
+    module('meanTweetsApp', function ($provide) {
+      $provide.value('currentUserFactory', currentUserFactoryMockSuccess);
+    });
 
     inject(function($injector) {
       $q = $injector.get('$q');
       var $controller = $injector.get('$controller');
       scope = $injector.get('$rootScope').$new();
       httpBackend = $injector.get('$httpBackend');
+      Restangular = $injector.get('Restangular');
+      currentUserFactory = $injector.get('currentUserFactory');
+      apiEndPointFactory = $injector.get('apiEndPointFactory');
 
 
-      ctrl = $controller('GlobalCtrl', { 
-        $scope: scope
+      ctrl = $controller('FollowUserCtrl', { 
+        $scope: scope,
+        $httpBackend: httpBackend,
+        currentUserFactory: currentUserFactory,
+        apiEndPointFactory: apiEndPointFactory
       });
     });
 
     httpBackend.whenGET(/views.*/).respond(200, '');
 
   });
+
 
 
 });
