4685445fc493e7c2d2af22bc1514109199ccb280
ttbarnes <tony@tonybarnes.me>
2015-08-24 20:38:06 +0100

    post tweets tests

--- test/api/routes.js
+++ test/api/routes.js
@@ -3,35 +3,57 @@ var assert = require('assert');
 var request = require('supertest');
 var mongoose = require('mongoose');
 
-describe('Routing', function() {
+var testTweet = {
+  username: 'bill',
+  copy: 'api test tweet!',
+  image:{
+    url: 'http://random.org/someimage.jpg'
+  },
+  timestamp: new Date().toISOString()
+};
 
-  var url = 'http://localhost:2000';
+describe('tweets', function() {
+
+  var url = 'http://localhost:2000/';
 
   before(function(done) {
     mongoose.connect('ttbarnes:j3zuNAWkwLMz@ds049641.mongolab.com:49641/mean-example-db');
     done();
   });
 
-  describe('Account', function() {
 
-    it('should return error trying to save duplicate username', function (done) {
+  describe('POST', function(){
 
-      var username = 'bill';
-
+    it('should be successful have return a message', function (done){
       request(url)
-        .get('/api/profiles/' + username)
-        //end handles the response
-        .end(function (err, res) {
+        .post('api/tweets')
+        .send(testTweet)
+        .end(function (err, res){
           if (err) {
-              throw err;
+            throw err;
           }
-          //res.should.have.status(200);
-          res.body.should.be.instanceOf(Array);
+          res.should.have.property('status', 200);
+          res.body.should.have.property('message', 'Tweet created!');
           done();
-        });
+        })
+    });
+
+    it('should have with the correct properties', function (done){
+      request(url)
+        .post('api/tweets')
+        .send(testTweet)
+        .end(function (err, res){
+          if (err) {
+            throw err;
+          }
+          res.request._data.should.have.property('username', testTweet.username);
+          res.request._data.should.have.property('copy', testTweet.copy);
+          res.request._data.should.have.property('timestamp');
+          res.request._data.should.have.propertyByPath('image', 'url').eql(testTweet.image.url);
+          done();
+        })
     });
 
   });
-
 
 });
