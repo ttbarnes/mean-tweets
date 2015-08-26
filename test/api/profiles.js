b26cb9da0e8c5579d69d2707bc20fa7a6fe732d5
ttbarnes <tony@tonybarnes.me>
2015-08-26 14:45:44 +0100

    modules for api tests

---
+++ test/api/profiles.js
@@ -0,0 +1,14 @@
+var should   = require('should');
+var assert   = require('assert');
+var request  = require('supertest');
+var mongoose = require('mongoose');
+
+var testProfiles = function(){
+
+  describe('profiles', function() {
+
+  });
+
+};
+
+exports = testProfiles();
